03563592b4cda9f48f15f6c3b127826fb1cd160a
USER <spicy___noodle@naver.com>
2021-05-25 13:18:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test01/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.test01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+         val data = editTextTextPersonName.text.toString()
+
+            intent
+        }
+
+
+
+    }
+}
